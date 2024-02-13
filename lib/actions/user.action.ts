5ce4cd28e4d5e481aa54701329f5f4be089637b5
Tuấn Lê <n19dccn177@student.ptithcm.edu.vn>
2024-02-13 20:56:54 +0700

    feat: add webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getUserById = async (params: any) => {
@@ -11,6 +18,56 @@ export const getUserById = async (params: any) => {
     const user = await User.findOne({ clerkId: userId });
 
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const createUser = async (params: CreateUserParams) => {
+  try {
+    connectToDatabase();
+    await User.create(params);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateUser = async (params: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+
+    await User.findByIdAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteUser = async (params: DeleteUserParams) => {
+  try {
+    connectToDatabase();
+    const { clerkId } = params;
+
+    const userToDelete = await User.findOne({ clerkId });
+    if (!userToDelete) {
+      throw new Error("User not found!");
+    }
+
+    // Delete all user's questions, answers, comments,...
+
+    // Get User's questions ids
+    // const userQuestionsId = await Question.find({
+    //   author: userToDelete._id,
+    // }).distinct("_id");
+
+    await Question.deleteMany({ author: userToDelete._id });
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
